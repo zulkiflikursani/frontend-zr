4120b4bc64b66040137d3ad8bb8857728d34df87
zulkifli kursani <zulkiflikursani@gmail.com>
2023-06-04 12:44:29 +0800

    remove readonly hjual

--- src/componens/Penjualan.jsx
+++ src/componens/Penjualan.jsx
@@ -145,7 +145,7 @@ const Penjualan = () => {
             <input
               className="appearance-none block w-full bg-white-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-last-name"
-              value={selectedOption}
+              value={hargajual}
             />
           </div>
           <div className="w-full md:w-1/2 px-3 my-2">
